e72070ec18e63185f99418a5d3b7fc51b063bb7d
CodingStorageSoo <chlqudtn347@gmail.com>
2023-08-05 16:11:02 -0700

    Reconcile Workspace

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,10 @@
+// go mod init broker
+package main
+
+// go get github.com/go-chi/chi/v5
+// go get github.com/go-chi/chi/v5/middleware
+// go get github.com/go-chi/cors
+
+func main() {
+
+}	
---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func routes () http.Handler {
+
+	mux := chi.NewRouter()
+
+} 
