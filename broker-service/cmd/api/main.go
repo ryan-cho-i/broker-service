bf009315775a3ddb24e4d5be6f67f751bf65c497
CodingStorageSoo <chlqudtn347@gmail.com>
2023-08-05 23:57:44 -0700

    Add Broker Part

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,10 +1,33 @@
 // go mod init broker
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 // go get github.com/go-chi/chi/v5
 // go get github.com/go-chi/chi/v5/middleware
 // go get github.com/go-chi/cors
 
-func main() {
+const webPort = "80"
 
+type Config struct {}
+
+func main() {
+	
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic((err))
+	}
 }	
--- broker-service/go.mod
+++ broker-service/go.mod
@@ -3,6 +3,7 @@ module broker
 go 1.20
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 )
